275c9d1f849d2ed38004e4901fac1df4b16902c4
Lalith Kiran .S <153124688+lalithkirangithub@users.noreply.github.com>
2025-11-19 20:12:49 +0530

    Update Footer.jsx

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
         <div className="md:max-w-96">
             <img className="h-9" src={assets.logo} alt="logo" />
             <p className="mt-6 text-sm" >
-                Experience the power of AI with QuickAi. <br /> Transfrom your content creation with our suite of premium AI
+                Experience the power of AI with SparkAi. <br /> Transfrom your content creation with our suite of premium AI
                 tools. Write articles, generate images, and enhance your workflow.
             </p>
         </div>
